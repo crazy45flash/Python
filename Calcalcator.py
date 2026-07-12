4c4596db9c50342fd90602a78842987114dde77a
crazy45flash <marthimanjunadh@gmail.com>
2026-07-12 18:23:57 +0530

    Update Calcalcator.py

--- Calcalcator.py
+++ Calcalcator.py
@@ -1,5 +1,12 @@
-a=int(input("number 1"))
-b=int(input("number 2"))
+a=int(input("number 1 "))
+b=int(input("number 2 ")) 
 c=input(" sign +,-,*,/ ")
 if c== "+" :
    print(a + b)
+elif c== "-" :
+   print(a - b)
+elif c== "*" :
+   print(a * b)
+elif c== "/" :
+   print(a / b)
+   
