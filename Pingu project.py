d740d9ed3f4b65ede461717438973210a2b37bdb
crazy45flash <marthimanjunadh@gmail.com>
2026-07-12 20:13:23 +0530

    Update Pingu project.py

--- Pingu project.py
+++ Pingu project.py
@@ -1,1 +1,31 @@
+def add_more():
+  print("add more"), input=input("y / n")
+  if input==y:
+    repeat(1):
+    item
+    repeat
+
+def add_more2():
+  print("add more"), input=input("y / n")
+  if input==y:
+    repeat(1):
+    price
+    repeat
+def add_more1():
+  print("add more"), input=input("y / n")
+  if input==y:
+    repeat(1):
+    quantity
+    repeat
+def repeat():
+   input=input("y / n")
+  if input==y
+    repeat(1)
+item =input("enter name of the product")
+add_more
+quantity=int(input("number of the product")
+add_more1            
+price=int(input("price of the product")
+add_more2
+total=(quantity * price * (tax/100)+(quantity * price  )
 
